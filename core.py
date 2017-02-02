0ff370e40422acbb4f628c5254b5a5f93fc16312
basecamp <jspence@basecampcodingacademey.org>
2017-02-02 16:07:50 -0600

    added total function and is somewhat functional

--- core.py
+++ core.py
@@ -1,3 +1,8 @@
+""" this program is to better the inventory of a rental place and
+also better the customer interface"""
+
+from datetime import datetime
+
 def inventory():
     '''none -> list of inventory'''
     with open('inventory.txt', 'r') as file:
@@ -6,10 +11,34 @@ def inventory():
         print(inv)
 
 def total():
-    input()
-# def damage():
+    '''none -> int '''
+    name = input("give the name of the thing rented \t")
+    types = input("give the type of rental \t")
+    if types == "vhs":
+        price = 2.00
+        repval = 5.00
+    elif types == 'dvd':
+        price = 5.00
+        repval = 20.00
+    elif types == 'blu-ray':
+        price = 8.00
+        repval = 25.00
+    elif types == 'special':
+        price = 10.00
+        repval = 30.00
+    elif types == 'comic':
+        price = 10.00
+        repval = 30.00
+
+    date = "{:%B %d, %Y}".format(datetime.now())
+    totals = int(price) + (int(repval) * 0.1)
+    reciept = str(date) + ' ' + name + " " + types + " " + str(totals)
+    print(reciept)
+
+
 
 def main():
+    ''' the main function that houses all the other ones'''
     user = input("Am I talking to a customer or a employee?\t")
     if user == 'customer':
         inven = input("Would you like to see our inventory?\t")
@@ -24,8 +53,8 @@ def main():
             if job == 'rent':
                 inventory()
                 total()
-            # else job == 'return':
-            #     # damage()
+            elif job == 'return':
+                input("is the rental damaged?")
 
 
 if __name__ == '__main__':
