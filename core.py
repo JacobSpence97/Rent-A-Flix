19e7928fd259776cf5b59abb155d42d46b790241
basecamp <jspence@basecampcodingacademey.org>
2017-02-03 13:18:36 -0600

    rewrote a few functions so the will be testable later on

--- core.py
+++ core.py
@@ -3,59 +3,21 @@ also better the customer interface"""
 
 from datetime import datetime
 
-def inventory():
-    '''none -> list of inventory'''
-    with open('inventory.txt', 'r') as file:
+def inventory(filename):
+    '''none -> list of inventory
+    takes a file name and prints it out for the user to see'''
+    with open(filename, 'r') as file:
         inv = file.readlines()
         inv = [i.replace('\n', '') for i in inv]
-        print(inv)
+        return inv
 
-def total():
-    '''none -> int '''
-    name = input("give the name of the thing rented \t")
-    types = input("give the type of rental \t")
-    if types == "vhs":
-        price = 2.00
-        repval = 5.00
-    elif types == 'dvd':
-        price = 5.00
-        repval = 20.00
-    elif types == 'blu-ray':
-        price = 8.00
-        repval = 25.00
-    elif types == 'special':
-        price = 10.00
-        repval = 30.00
-    elif types == 'comic':
-        price = 10.00
-        repval = 30.00
-
+def total(name, types, price, repval):
+    '''none -> int 
+    finds the total of the final bill and prints a reciept'''
     date = "{:%B %d, %Y}".format(datetime.now())
     totals = int(price) + (int(repval) * 0.1)
     reciept = str(date) + ' ' + name + " " + types + " " + str(totals)
-    print(reciept)
+    return reciept
 
 
 
-def main():
-    ''' the main function that houses all the other ones'''
-    user = input("Am I talking to a customer or a employee?\t")
-    if user == 'customer':
-        inven = input("Would you like to see our inventory?\t")
-        if inven == 'yes':
-            inventory()
-        elif inven == 'no':
-            print('Please see an employee')
-        else:
-            main()
-    elif user == 'employee':
-            job = input("What is the customer wanting to do? rent or return\t")
-            if job == 'rent':
-                inventory()
-                total()
-            elif job == 'return':
-                input("is the rental damaged?")
-
-
-if __name__ == '__main__':
-    main()
