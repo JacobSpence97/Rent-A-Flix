a5bd6e7f0462babc6f40dd3b515907279db1b069
basecamp <jspence@basecampcodingacademey.org>
2017-02-03 15:07:14 -0600

    test for total function writen

---
+++ test.py
@@ -0,0 +1,7 @@
+'''this file is to test all functions in the main core file'''
+import core
+
+def test_total():
+    '''this test testes the function total'''
+    j = core.total('test', 'Test', 2.00, 5.00)
+    assert j == 'February 03, 2017 test Test 2.5'
