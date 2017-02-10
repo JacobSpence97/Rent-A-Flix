92457c0311c9c19d9fb75154f7bba6bb74686287
basecamp <jspence@basecampcodingacademey.org>
2017-02-10 13:07:56 -0600

    new transactions and fixed to work with functions

--- rent_a_flix.py
+++ rent_a_flix.py
@@ -1,5 +1,6 @@
 """ this program is to better the inventory of a rental place and
 also better the customer interface"""
+import datetime
 import core
 
 def main():
@@ -14,7 +15,7 @@ def main():
         else:
             main()
     elif user == 'employee':
-        job = input("What is the customer wanting to do? rent or return\t")
+        job = input("What is the customer wanting to do? rent or return or inv?\t").strip().lower()
         if job == 'rent':
             print(core.inventory('inventory.txt'))
             name = input("give the name of the item being rented \t")
@@ -34,7 +35,32 @@ def main():
             elif types == 'comic':
                 price = 10.00
                 repval = 30.00
-            print(core.total('transaction.txt', name, types, price, repval))
+            date = "{:%B %d, %Y}".format(datetime.datetime.now())
+            print(core.total('transaction.txt', name, date, types, price, repval))
+        elif job == 'return':
+            damage = input('is the rental damaged?\t')
+            if damage == 'yes':
+                types = input("give the type of rental \t")
+                if types == "vhs":
+                    repval = 5.00
+                elif types == 'dvd':
+                    repval = 20.00
+                elif types == 'blu-ray':
+                    repval = 25.00
+                elif types == 'special':
+                    repval = 30.00
+                elif types == 'comic':
+                    repval = 30.00
+                return repval
+            elif damage == 'no':
+                print('please return the customers repval deposit.')
+        elif job == 'inv':
+            print(core.inventory('inventory.txt'))
+            print('Make sure the item isnt already in the inventory')
+            item_name = input('what is the name of the item being added?\t')
+            core.add_inv('inventory.txt', item_name)
+        else:
+            main()
 
 
 if __name__ == '__main__':
