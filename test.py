160cd0007ee19e7d6fbcbc94d9999ebe7ed54e53
basecamp <jspence@basecampcodingacademey.org>
2017-02-10 13:06:08 -0600

    fixed incorrect test

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import core
 
 def test_total():
     '''this test testes the function total'''
-    j = core.total('test', 'Test', 2.00, 5.00)
+    j = core.total('filename', 'test', 'February 03, 2017', 'Test', 2.00, 5.00)
     assert j == 'February 03, 2017 test Test 2.5'
