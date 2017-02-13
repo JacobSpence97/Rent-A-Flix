5c5a4173cdfa393f10a37fd20bc6cddf7d29ead5
basecamp <jspence@basecampcodingacademey.org>
2017-02-13 08:57:48 -0600

    cleaned up the code some and added also added some test corrections to the functions

--- core.py
+++ core.py
@@ -5,25 +5,26 @@ also better the customer interface"""
 def inventory(filename):
     '''(string) -> (list of inventory)
     takes a file name and prints it out for the user to see'''
+    string = ''
     with open(filename, 'r') as file:
         inv = file.readlines()
         for item in inv[1:]:
-            return item
+            string += item + '\n'
+        return string
 
 
 def total(filename, name, date, types, price, repval):
     ''' (string,string,string,int,int) -> (string)
     finds the total of the final bill and prints a reciept'''
     totals = int(price) + (int(repval) * 0.1)
-    reciept = (str(date) + ' ' + name + " " + types + " " + str(totals)+ '\n')
+    reciept = (str(date) + ' ' + name + " " + types + " " + str(totals) + '\n')
     with open(filename, 'a') as file:
         file.write(reciept)
     return reciept
 
-def add_inv(filename, item_name):
+def add_inv(filename, item_name, types):
     """(string, string) -> (string)
     adds new inventory to the inventory file"""
-    types = input('what type of rental is it?\t')
     if types == 'vhs':
         price = 2.00
         repval = 5.00
@@ -39,6 +40,20 @@ def add_inv(filename, item_name):
     elif types == 'comic':
         price = 10.00
         repval = 30.00
+    else:
+        print("invalad type")
+        add_inv(filename, item_name, types)
+
     item = (item_name + ', ' + types + ', ' + str(price) + ', ' + str(repval) + '\n')
     with open(filename, 'a') as file:
         file.write(item)
+    return item
+
+add_inv('test.txt', 'batman', 'dvd')
+# def removeinv(name, types, price, repval):
+#     ''' (strings) -> None
+#     removes item from inventory when rented'''
+#     inventory('inventory.txt')
+#     item = (name, types, price, repval)
+
+#     if item == 
