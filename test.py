ce9d27bc0790e1ac950f030a38e7f487146d86ec
basecamp <jspence@basecampcodingacademey.org>
2017-02-15 09:32:59 -0600

    fixed some test and added some new test

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ def test_inventory():
         file.write('test, Test, TeSt\n')
         file.write('test, Test, TeSt')
     k = core.inventory('test.txt')
-    assert k == 'test, Test, TeSt\n'
+    assert k == 'test, Test, TeSt\ntest, Test, TeSt'
     os.remove('test.txt')
 
 def test_add_inv():
@@ -31,4 +31,23 @@ def test_add_inv():
         file.write('test, Test, TeSt\n')
     vari = core.add_inv('test.txt', 'batman', 'dvd')
     assert vari == 'batman, dvd, 5.0, 20.0\n'
+    os.remove('test.txt')
+
+
+def test_removeinv():
+    '''test the function removeinv'''
+    create_test_file('test.txt')
+    with open('test.txt', 'a') as file:
+        file.write('test,Test,5.00,20.00')
+    vari = core.removeinv('test.txt','test','Test',5.00,20.00)
+    assert vari == ''
+    os.remove('test.txt')
+
+def test_returninv():
+    '''tests the function returninv'''
+    create_test_file('test.txt')
+    with open('test.txt', 'a') as file:
+        file.write('test,Test,5.00,20.00')
+    vari = core.returninv('test.txt','test','Test',5.00,20.00)
+    assert vari == 'test,Test,5.00,20.00'
     os.remove('test.txt')
