690522c0c46ad19f96188add504928485f2712b0
basecamp <jspence@basecampcodingacademey.org>
2017-02-15 10:46:38 -0600

    added a few if statements that i forgot

--- rent_a_flix.py
+++ rent_a_flix.py
@@ -19,7 +19,7 @@ def main():
         if job == 'rent':
             print(core.inventory('inventory.txt'))
             name = input("give the name of the item being rented \t")
-            types = input("give the type of rentalvhs,dvd,blu-ray,special,comic \t")
+            types = input("give the type of rental?  vhs,dvd,blu-ray,special,comic \t")
             if types == "vhs":
                 price = 2.00
                 repval = 5.00
@@ -42,14 +42,31 @@ def main():
             date = "{:%B %d, %Y}".format(datetime.datetime.now())
             print(core.total('transaction.txt', name, date, types, price, repval))
         elif job == 'return':
-            types = input('what is the type of the rental?')
-            damage =input('was there any damage to the rental?')
-            core.revenue('tevenue.txt', types, damage)
+            name = input('what is the name of the renturning rental?\t')
+            types = input('what is the type of the rental?\t')
+            damage =input('was there any damage to the rental?\t')
+            if types == "vhs":
+                price = 2.00
+                repval = 5.00
+            elif types == 'dvd':
+                price = 5.00
+                repval = 20.00
+            elif types == 'blu-ray':
+                price = 8.00
+                repval = 25.00
+            elif types == 'special':
+                price = 10.00
+                repval = 30.00
+            elif types == 'comic':
+                price = 10.00
+                repval = 30.00
+            core.revenue('revenue.txt', types, damage)
+            core.returninv('inventory.txt', name, types, price, repval)
         elif job == 'inv':
             print(core.inventory('inventory.txt'))
             print('Make sure the item isnt already in the inventory')
             item_name = input('what is the name of the item being added?\t')
-            types = input('what is the type of rental?vhs,dvd,blu-ray,special,comic\t')
+            types = input('what is the type of rental?  vhs,dvd,blu-ray,special,comic\t')
             core.add_inv('inventory.txt', item_name, types)
         else:
             main()
